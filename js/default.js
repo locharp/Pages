e415135c6d1ade7a55f6ade471b04287ba36aec6
locharp <lochardpatrick@outlook.com>
2023-01-28 01:56:50 +0200

    c

--- js/default.js
+++ js/default.js
@@ -1,10 +1,11 @@
+'use strict';
+
 function showRandom(className) {
   let elements = document.getElementsByClassName(className);
   for (let i = 0; i < elements.length; i++)
-    elements[i].style.visibility = 'collapse';
-  elements[random(elements.length)].visibility = 'visible';
+    elements[i].style.display = 'none';
+  elements[Math.floor(Math.random() * elements.length)].style.display = 'inline';
 }
-
 
 onload = () => {
   showRandom('quotes');
