d4baeb82382829a3fab3f9470f7c64115caba083
locharp <lochardpatrick@outlook.com>
2023-01-28 00:30:36 +0200

    c

--- js/default.js
+++ js/default.js
@@ -1,3 +1,11 @@
-(function () {
-  alert("working");
-}());
+function showRandom(className) {
+  let elements = document.getElementsByClassName(className);
+  for (let i = 0; i < elements.length; i++)
+    elements[i].style.visibilty = collapse;
+  elements[random(elements.length)].visibility = 'visible';
+}
+
+
+onload = () => {
+  showRandom('quotes');
+}
