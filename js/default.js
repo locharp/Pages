9ae7f61ac03534968e672c795e3496d2b11bcd32
locharp <lochardpatrick@outlook.com>
2023-01-28 01:22:30 +0200

    c

--- js/default.js
+++ js/default.js
@@ -1,7 +1,7 @@
 function showRandom(className) {
   let elements = document.getElementsByClassName(className);
   for (let i = 0; i < elements.length; i++)
-    elements[i].style.visibilty = collapse;
+    elements[i].style.visibilty = 'collapse';
   elements[random(elements.length)].visibility = 'visible';
 }
 
