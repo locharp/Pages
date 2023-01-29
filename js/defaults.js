c96b725a2937d712fdfaab8df582641a95538989
locharp <lochardpatrick@outlook.com>
2023-01-29 15:30:29 +0200

    c

--- js/defaults.js
+++ js/defaults.js
@@ -1,16 +1,19 @@
 'use strict';
 
-function hideAll(className) {
+function hideAll(className)
+{
   let elements = document.getElementsByClassName(className);
   for (let i = 0; i < elements.length; i++)
     elements[i].hidden = 'hidden';
 }
 
-function showRandom(className) {
+function showRandom(className)
+{
   let elements = document.getElementsByClassName(className);
   elements[Math.floor(Math.random() * elements.length)].removeAttribute('hidden');
 }
 
-onload = () => {
+onload = () =>
+{
   showRandom('quote');
 }
