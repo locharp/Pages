1c30bbcedbb83846c11569da8f63fbad4f30e4ca
Lochard <lochadpatrick@outlook.com>
2023-05-08 23:03:34 +0300

    test

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 import React from 'js/react.development.js';
 import ReactDOM from 'js/react-dom.development.js';
+
 function Hello()
 {
-    return <h1>Hello World!</h1>;
+    return <h1>Hello world</h1>;
 }
 
 
-const root = ReactDOM.createRoot(document.getElementById('app'));
+const container = document.getElementById('app');
+const root = ReactDOM.createRoot(container);
 root.render(<Hello />);
